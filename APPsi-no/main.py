cc4f88cfb72980626e06669676c5599560f94764
may873 <vitalliquidanoemmanuel@cetis50cdmx.com>
2024-09-05 18:30:03 -0600

    subiendo version 1.0 de app si-no

---
+++ APPsi-no/main.py
@@ -0,0 +1,12 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title="Me perdonas "
+    page.bgcolor="pink"
+    page.horizontal_alignment=ft.CrossAxisAlignment.center
+    page.vertical_alignment=ft.MainAxisAlignment.center
+
+
+
+ft.app(main)
