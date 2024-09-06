87bd8ca6a53dd9780b6b4e6e20652343ad484f93
may873 <vitalliquidanoemmanuel@cetis50cdmx.com>
2024-09-05 19:16:20 -0600

    se sube vercion 1.2

--- APPsi-no/main.py
+++ APPsi-no/main.py
@@ -2,11 +2,34 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.title="Me perdonas "
+    page.title="¿Me perdonas?"
     page.bgcolor="pink"
-    page.horizontal_alignment=ft.CrossAxisAlignment.center
-    page.vertical_alignment=ft.MainAxisAlignment.center
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER 
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
 
-
+    lbl1=ft.Text("¿Me perdonas?",
+                style=ft.TextStyle(size=40,weight="blod"))
+    ing1=ft.image(src="triste.png",weight=200,height=200)
+    btnSi=ft.ElevatedButton("Si",
+                            color="green",
+                            width=100,
+                            height=50
+                            )
+    btnNo=ft.ElevatedButton("No",
+                            color="red",
+                            width=100,
+                            height=50
+                            )
+    page.add(
+        ft.Column(
+            [
+                lbl1,
+                ing1,
+                ft.Row(
+                    [btnSi,btnNo]
+                )
+            ]
+        )
+    )
 
 ft.app(main)
